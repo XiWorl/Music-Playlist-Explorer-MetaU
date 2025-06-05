62712fdd6146be69b87ca50ec5e65f49bfb2317c
Uriel Chinemerem Vit-Ojiegbe <uvit@meta.com>
2025-06-05 14:43:32 -0700

    A lot

---
+++ music-playlist-creator/info.js
@@ -0,0 +1,280 @@
+// let playlist = [
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/playlist.png",
+//         "playlist_likes": 5,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     },
+//     {
+//         "playlistID": 12123,
+//         "playlist_name": "abcd",
+//         "playlist_author": "John doe",
+//         "playlist_art": "assets/img/song.png",
+//         "playlist_likes": 10,
+//         "songs": {
+//             "song name1": {
+//                 duration: "3:44",
+//                 author: "author name",
+//                 album: "album name",
+//                 img: "assets/img/song.png"
+//             },
+//             "song name2": {
+//                 duration: "5:11",
+//                 author: "author name2",
+//                 album: "album name2",
+//                 img: "assets/img/song.png"
+//             },
+//         }
+//     }
+// ]
+
+
+
+let playlist = [
+    {
+    "playlistID": 1,
+        "playlist_name": "Morning Motivation",
+        "playlist_author": "Emily Chen",
+        "playlist_art": "assets/img/playlist.png",
+        "playlist_likes": 5,
+        "songs": {
+            "Wake Me Up": {
+                duration: "3:44",
+                author: "Avicii",
+                album: "True",
+                img: "assets/img/song.png"
+            },
+            "Can't Stop": {
+                duration: "5:11",
+                author: "Red Hot Chili Peppers",
+                album: "By the Way",
+                img: "assets/img/song.png"
+            },
+        }
+    },
+    {
+        "playlistID": 2,
+        "playlist_name": "Relaxing Evening",
+        "playlist_author": "David Lee",
+        "playlist_art": "assets/img/song.png",
+        "playlist_likes": 10,
+        "songs": {
+            "River Flows in You": {
+                duration: "3:38",
+                author: "Yiruma",
+                album: "First Love",
+                img: "assets/img/song.png"
+            },
+            "Weightless": {
+                duration: "4:28",
+                author: "Marconi Union",
+                album: "Gravity",
+                img: "assets/img/song.png"
+            },
+        }
+    },
+    {
+        "playlistID": 3,
+        "playlist_name": "Workout Mix",
+        "playlist_author": "Sarah Taylor",
+        "playlist_art": "assets/img/song.png",
+        "playlist_likes": 15,
+        "songs": {
+            "Eye of the Tiger": {
+                duration: "3:59",
+                author: "Survivor",
+                album: "Eye of the Tiger",
+                img: "assets/img/song.png"
+            },
+            "Lose Yourself": {
+                duration: "5:26",
+                author: "Eminem",
+                album: "8 Mile: Music from and Inspired by the Motion Picture",
+                img: "assets/img/song.png"
+            },
+        }
+    },
+    {
+        "playlistID": 4,
+        "playlist_name": "Throwback Thursday",
+        "playlist_author": "Michael Brown",
+        "playlist_art": "assets/img/song.png",
+        "playlist_likes": 20,
+        "songs": {
+            "I Want It That Way": {
+                duration: "3:33",
+                author: "Backstreet Boys",
+                album: "Millennium",
+                img: "assets/img/song.png"
+            },
+            "Waterfalls": {
+                duration: "4:39",
+                author: "TLC",
+                album: "CrazySexyCool",
+                img: "assets/img/song.png"
+            },
+        }
+    },
+    {
+        "playlistID": 5,
+        "playlist_name": "Focus Flow",
+        "playlist_author": "Jessica White",
+        "playlist_art": "assets/img/song.png",
+        "playlist_likes": 12,
+        "songs": {
+            "Clair de Lune": {
+                duration: "4:55",
+                author: "Claude Debussy",
+                album: "Preludes, Book 1",
+                img: "assets/img/song.png"
+            },
+            "Moonlight Sonata": {
+                duration: "5:23",
+                author: "Ludwig van Beethoven",
+                album: "Piano Sonata No. 14",
+                img: "assets/img/song.png"
+            },
+        }
+    }
+]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,145 @@
+// async function getJsonData() {
+//   try {
+//     const response = await fetch('./data.json');
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     return await response.json();
+//   } catch (error) {
+//     console.error('Error fetching JSON:', error);
+//   }
+// }
 
+
+// function populatePlaylists() {
+//     console.log(playlist)
+//     let jsData
+//     getJsonData().then(function(data) {
+//         jsData = data
+
+//         for (let i = 0; i < jsData.length; i++) {
+//             let clone = document.querySelector("#song-template").cloneNode(true)
+//             document.getElementById("music-holder").appendChild(clone)
+            
+//             clone.style.visibility = "visible"
+//             clone.id = ""
+
+//             for (let child of clone.children) {
+//                 if (child.nodeName === "H3") {
+//                     child.innerHTML = jsData[i].playlist_name
+//                 }else if (child.nodeName === "P") {
+//                     child.innerHTML = jsData[i].playlist_author
+//                 }else if (child.nodeName === "IMG") {
+//                     child.src = jsData[i].playlist_art
+//                 }
+//             }
+
+//             clone.addEventListener("click", function() {
+//                 console.log(document.querySelector(".modal"))
+//                 document.querySelector(".modal").style.visibility = "visible"
+//             })
+//         }
+//         document.querySelector("#song-template").remove()
+//     })
+//     return jsData
+// }
+
+function showPlaylistInfo(songTable) {
+    // console.log(songTable)
+    
+    // for (let child of document.querySelector("#modal-bottom").children) {
+    //     console.log(child)
+    //     if (child.id !== "modal-songs") {
+    //         child.remove()
+    //     }
+    // }
+    
+    let saveList = []
+
+    console.log(songTable)
+    for (let songName in songTable) { 
+        let alternate = true
+        let clone = document.querySelector("#modal-songs").cloneNode(true)
+        document.getElementById("modal-bottom").appendChild(clone)
+        // console.log(clone)
+        // clone.id = ""
+
+        for (let child of clone.children) {
+            if (child.nodeName === "IMG") {
+                child.src = songTable[songName].img
+            }else if (child.nodeName === "P") {
+                child.innerHTML = songTable[songName].duration
+            } else if (child.nodeName == "DIV") {
+                
+                for (let divChild of child.children) {
+                    if (divChild.nodeName === "H4") {
+                        divChild.innerHTML = songName
+                    } else if (divChild.nodeName === "P" && alternate === true) {
+                        divChild.innerHTML = songTable[songName].author
+                        alternate = false
+                    }else if (divChild.nodeName === "P" && alternate === false) {
+                        divChild.innerHTML = songTable[songName].album
+                    }
+                }
+            }
+        }
+        saveList.push(clone)
+    }
+
+    for (let i = 0; i < 2; i++) {
+        for (let child of document.querySelector("#modal-bottom").children) {
+            if (saveList.indexOf(child) === -1) {
+                child.remove()
+            }
+        }
+    }
+}
+
+
+function shufflePlaylist() {
+    //replace children
+    let container = document.querySelector("#modal-bottom")
+    for (let i = container.children.length; i >= 0; i--) {
+        container.appendChild(container.children[Math.random() * i | 0]);
+    }
+}
+
+function populatePlaylists() {
+    for (let i = 0; i < playlist.length; i++) {
+        let clone = document.querySelector("#song-template").cloneNode(true)
+        document.getElementById("music-holder").appendChild(clone)
+        
+        clone.style.visibility = "visible"
+        clone.id = ""
+
+        for (let child of clone.children) {
+            if (child.nodeName === "H3") {
+                child.innerHTML = playlist[i].playlist_name
+            }else if (child.nodeName === "P") {
+                child.innerHTML = "Created by "+playlist[i].playlist_author
+            }else if (child.nodeName === "IMG") {
+                child.src = playlist[i].playlist_art
+            }
+        }
+
+        clone.addEventListener("click", function() {
+            document.querySelector(".modal").style.visibility = "visible"
+            showPlaylistInfo(playlist[i].songs)
+        })
+    }
+    document.querySelector("#song-template").remove()
+}
+
+
+
+document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector(".modal").style.visibility = "hidden"
+})
+
+
+populatePlaylists()
+
+
+document.querySelector("#modal-button").addEventListener("click", function() {
+    shufflePlaylist()
+})
